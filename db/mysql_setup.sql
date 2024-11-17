c274fa784cf8c4d19212be5bb21e26848840cda2
yutaorii <oril.gonzales@icloud.com>
2024-11-17 19:09:31 +0900

    修正致しました。よろしくお願いいたします。

--- db/mysql_setup.sql
+++ db/mysql_setup.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS daily_report_system CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 CREATE USER IF NOT EXISTS 'repuser'@'%' IDENTIFIED BY 'reppass';
-GRANT ALL PRIVILEGES ON *.* TO 'devuser'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'repuser'@'%';
 
 
 
